8d04a5925529f2ef579721e23352f6cbe6b2ea4a
koipkoi <hrh6914@gmail.com>
2022-01-14 10:38:32 +0900

    TransparentNavigationBarAndroid 추가

--- android/src/main/java/com/nextstory/nextstory/NextstoryPlugin.java
+++ android/src/main/java/com/nextstory/nextstory/NextstoryPlugin.java
@@ -1,6 +1,14 @@
 package com.nextstory.nextstory;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+
+import java.lang.reflect.Field;
+import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
@@ -22,10 +30,55 @@ public class NextstoryPlugin implements FlutterPlugin, MethodCallHandler {
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        switch (call.method) {
+            case "initializeActivity":
+                Activity activity = Utils.getActivity();
+                if (activity != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
+                }
+                break;
+        }
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
+    }
+}
+
+class Utils {
+    private Utils() {
+        // no-op
+    }
+
+    @SuppressLint("PrivateApi")
+    @SuppressWarnings("unchecked")
+    public static Activity getActivity() {
+        try {
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            Object activityThread = activityThreadClass.getMethod("currentActivityThread")
+                    .invoke(null);
+            Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+            activitiesField.setAccessible(true);
+
+            Map<Object, Object> activities =
+                    (Map<Object, Object>) activitiesField.get(activityThread);
+            if (activities == null) {
+                return null;
+            }
+
+            for (Object activityRecord : activities.values()) {
+                Class<?> activityRecordClass = activityRecord.getClass();
+                Field pausedField = activityRecordClass.getDeclaredField("paused");
+                pausedField.setAccessible(true);
+                if (!pausedField.getBoolean(activityRecord)) {
+                    Field activityField = activityRecordClass.getDeclaredField("activity");
+                    activityField.setAccessible(true);
+                    return (Activity) activityField.get(activityRecord);
+                }
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 }
