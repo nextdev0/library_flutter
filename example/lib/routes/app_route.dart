0531048f4bec39fbb8ba2d774e9ce2c725fed1ed
koipkoi <hrh6914@gmail.com>
2022-01-18 15:59:25 +0900

    example 프로젝트 업데이트
    라이브러리 변경에 따른 수정

--- example/lib/routes/app_route.dart
+++ example/lib/routes/app_route.dart
@@ -1,10 +1,10 @@
 import 'package:nextstory/foundation/navigator_change_notifier.dart';
-import 'package:nextstory/widgets/simple_route.dart';
+import 'package:nextstory/widgets/simple_page_route.dart';
 import 'package:nextstory_example/pages/home.dart';
 
 class AppRoutes extends NavigatorChangeNotifier {
-  SimpleRoute get home {
-    return SimpleRoute(
+  SimplePageRoute get home {
+    return SimplePageRoute(
       popGestureEnabled: false,
       page: (_) => const HomePage(),
     );
