749ab39274dbf9767a43e07cf98fb494bf7ce3b1
Hwang Ryuha <hrh6914@gmail.com>
2022-02-25 21:26:38 +0900

    java indent 수 변경
    4 > 2로 변경

--- android/src/main/java/com/nextstory/nextstory/NextstoryPlugin.java
+++ android/src/main/java/com/nextstory/nextstory/NextstoryPlugin.java
@@ -20,71 +20,71 @@ import io.flutter.plugin.common.MethodChannel.Result;
  * NextstoryPlugin
  */
 public class NextstoryPlugin implements FlutterPlugin, MethodCallHandler {
-    private MethodChannel channel;
+  private MethodChannel channel;
 
-    @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "nextstory");
-        channel.setMethodCallHandler(this);
-    }
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "nextstory");
+    channel.setMethodCallHandler(this);
+  }
 
-    @Override
-    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-        switch (call.method) {
-            case "enableAndroidTransparentNavigationBar":
-                Activity activity = Utils.getActivity();
-                if (activity != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
-                }
-                result.success(null);
-                break;
-
-            default:
-            case "disableDelayTouchesBeganIOS":
-                result.success(null);
-                break;
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    switch (call.method) {
+      case "enableAndroidTransparentNavigationBar":
+        Activity activity = Utils.getActivity();
+        if (activity != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          activity.getWindow().setNavigationBarColor(Color.TRANSPARENT);
         }
-    }
+        result.success(null);
+        break;
 
-    @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(null);
+      default:
+      case "disableDelayTouchesBeganIOS":
+        result.success(null);
+        break;
     }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
 }
 
 class Utils {
-    private Utils() {
-        // no-op
-    }
+  private Utils() {
+    // no-op
+  }
 
-    @SuppressLint("PrivateApi")
-    @SuppressWarnings("unchecked")
-    public static Activity getActivity() {
-        try {
-            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
-            Object activityThread = activityThreadClass.getMethod("currentActivityThread")
-                    .invoke(null);
-            Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
-            activitiesField.setAccessible(true);
+  @SuppressLint("PrivateApi")
+  @SuppressWarnings("unchecked")
+  public static Activity getActivity() {
+    try {
+      Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+      Object activityThread = activityThreadClass.getMethod("currentActivityThread")
+          .invoke(null);
+      Field activitiesField = activityThreadClass.getDeclaredField("mActivities");
+      activitiesField.setAccessible(true);
 
-            Map<Object, Object> activities =
-                    (Map<Object, Object>) activitiesField.get(activityThread);
-            if (activities == null) {
-                return null;
-            }
-
-            for (Object activityRecord : activities.values()) {
-                Class<?> activityRecordClass = activityRecord.getClass();
-                Field pausedField = activityRecordClass.getDeclaredField("paused");
-                pausedField.setAccessible(true);
-                if (!pausedField.getBoolean(activityRecord)) {
-                    Field activityField = activityRecordClass.getDeclaredField("activity");
-                    activityField.setAccessible(true);
-                    return (Activity) activityField.get(activityRecord);
-                }
-            }
-        } catch (Exception ignored) {
-        }
+      Map<Object, Object> activities =
+          (Map<Object, Object>) activitiesField.get(activityThread);
+      if (activities == null) {
         return null;
+      }
+
+      for (Object activityRecord : activities.values()) {
+        Class<?> activityRecordClass = activityRecord.getClass();
+        Field pausedField = activityRecordClass.getDeclaredField("paused");
+        pausedField.setAccessible(true);
+        if (!pausedField.getBoolean(activityRecord)) {
+          Field activityField = activityRecordClass.getDeclaredField("activity");
+          activityField.setAccessible(true);
+          return (Activity) activityField.get(activityRecord);
+        }
+      }
+    } catch (Exception ignored) {
     }
+    return null;
+  }
 }
